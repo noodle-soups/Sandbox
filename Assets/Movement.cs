4a366bc1a6774160e5880137d4a11cdcb4568ff0
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2023-10-27 15:18:28 +0100

    commit

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
 
     float moveSpeed = 3f;
+    float moveSpeedFast = 6f;
+    float moveSpeedSlow = 1f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-        
+        if (other.tag == "Speed Boost - Fast" && moveSpeed != moveSpeedFast)
+        {
+            moveSpeed = moveSpeedFast;
+            Debug.Log("Speed Fast!");
+        }
+
+        if (other.tag == "Speed Boost - Slow" && moveSpeed != moveSpeedSlow)
+        {
+            moveSpeed = moveSpeedSlow;
+            Debug.Log("Speed Slow!");
+        }
     }
+
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Trigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("Trigger");    
+    }
+
+}
